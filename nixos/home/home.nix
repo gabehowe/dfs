7668707df0a6444031a067f070393cf40b64f7c2
Gabriel Howe <me@gabe.how>
2026-09-17 12:05:08 -0500

    move nvim correctly

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -14,11 +14,10 @@
   home.username = "gabri";
   home.homeDirectory = "/home/gabri";
 
+  home.file.".config/nvim/".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nvim";
   home.stateVersion = "25.05"; # Please read the comment before changing.
   targets.genericLinux.enable = true;
 
-  home.file = {
-  };
   xdg = {
     enable = true;
     userDirs = {
